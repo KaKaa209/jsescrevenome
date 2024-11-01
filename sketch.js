207cda3d9b2b16450699cb6c541ca0f3a7e395c5
KaKaa209 <kauan.moro@escola.pr.gov.br>
2024-10-31 23:25:07 -0300

    purple

--- sketch.js
+++ sketch.js
@@ -1,18 +1,15 @@
 function setup() {
-  createCanvas(600, 600);
-   background("black");
+  createCanvas(400, 400);
+  background("purple")
 }
 
-
 function draw() {
+  stroke("blue");
+  fill("red");
   
-  stroke ("blue");
-  fill ("red");
- 
   
-  // console.log(mouseIsPressed);
-  
-  if (mouseIsPressed){
+  if (mouseIsPressed) {
     rect(mouseX, mouseY, 20, 35);
   }
 }
+
